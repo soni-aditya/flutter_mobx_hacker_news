c44412e7bf55428fd70c33d8aaee7ead6e642d00
Aditya Soni <adityas@geekyants.com>
2019-01-29 09:24:48 +0530

    feature : added depencencies and did code cleanup

---
+++ lib/screens/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hacker News'),
+      ),
+      body: Center(
+        child: Text('Some Text'),
+      ),
+    );
+  }
+}
